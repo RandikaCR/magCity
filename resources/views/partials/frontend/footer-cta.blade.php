77a1c158c848ce401dcb5e3226f672dd5bd29e0e
Randika De Alwis <randika@motordat.com>
2026-07-21 01:25:32 +0530

    fix

--- resources/views/partials/frontend/footer-cta.blade.php
+++ resources/views/partials/frontend/footer-cta.blade.php
@@ -2,8 +2,8 @@
     <div class="container equal-wraper text-white py-5">
         <div class="row">
             <div class="col-sm-4 mb-4 mb-sm-0">
-                <p class="fw-medium font-24 ">Ready to Experience the Mag City Difference?</p>
-                <p class="">Book your appointment today and let our expert take care your vehicle </p>
+                <p class="fw-medium font-24 text-white">Ready to Experience the Mag City Difference?</p>
+                <p class="text-white">Book your appointment today and let our expert take care your vehicle </p>
             </div>
             <div class="col-sm-8">
                 <div class="row">
@@ -14,8 +14,8 @@
                                 <i class="ti-mobile font-30"></i>
                             </div>
                             <div class="col-9 col-sm-10">
-                                <p class="mb-0 fw-medium footer-banner-item-text-top">Book Online</p>
-                                <p class="mb-0 footer-banner-item-text-bottom">Quick and easy appointment booking.</p>
+                                <p class="mb-0 fw-medium footer-banner-item-text-top text-white">Book Online</p>
+                                <p class="mb-0 footer-banner-item-text-bottom text-white">Quick and easy appointment booking.</p>
                             </div>
                         </div>
                     </div>
@@ -26,12 +26,12 @@
                                 <i class="ti-mobile font-30"></i>
                             </div>
                             <div class="col-9 col-sm-10">
-                                <p class="mb-0 fw-medium footer-banner-item-text-top">Call Us</p>
-                                <p class="mb-2 footer-banner-item-text-bottom">Speak with our team</p>
-                                <p class="mb-0 footer-banner-item-text-bottom">Colombo: <span class="ms-2">0114734020</span></p>
-                                <p class="mb-0 footer-banner-item-text-bottom">Galle: <span class="ms-2">0912228388</span></p>
-                                <p class="mb-0 footer-banner-item-text-bottom">Nugegoda: <span class="ms-2">0112199799</span></p>
-                                <p class="mb-0 footer-banner-item-text-bottom">Ratmalana: <span class="ms-2">0112626333</span></p>
+                                <p class="mb-0 fw-medium footer-banner-item-text-top text-white">Call Us</p>
+                                <p class="mb-2 footer-banner-item-text-bottom text-white">Speak with our team</p>
+                                <p class="mb-0 footer-banner-item-text-bottom text-white">Colombo: <span class="ms-2">0114734020</span></p>
+                                <p class="mb-0 footer-banner-item-text-bottom text-white">Galle: <span class="ms-2">0912228388</span></p>
+                                <p class="mb-0 footer-banner-item-text-bottom text-white">Nugegoda: <span class="ms-2">0112199799</span></p>
+                                <p class="mb-0 footer-banner-item-text-bottom text-white">Ratmalana: <span class="ms-2">0112626333</span></p>
                             </div>
                         </div>
                     </div>
