32f577212d6e1d51bf564ad90d1f55ac00977b81
Randika De Alwis <randika@motordat.com>
2026-07-23 14:57:04 +0530

    counters update

--- resources/views/partials/frontend/counters.blade.php
+++ resources/views/partials/frontend/counters.blade.php
@@ -1,40 +1,40 @@
 <div class="section-full bg-white py-4" id="counters-section">
     <div class="container">
-        <div class="row">
-            <div class="col-sm-3 col-6 mb-3 mb-sm-0 counter-item-1">
-                <div class="row">
-                    <div class="col-3 col-sm-2 counter-item-image-area">
+        <div class="row justify-content-center">
+            <div class="col-sm-3 col-12 mb-3 mb-sm-0 counter-item-1">
+                <div class="row justify-content-center">
+                    <div class="col-2 col-sm-2 counter-item-image-area">
                         <img class="img-fluid" src="{{ asset('assets/common/images/icon-1.png') }}" alt="">
                     </div>
-                    <div class="col-9 col-sm-10">
+                    <div class="col-8 col-sm-10">
                         <p class="mb-0 fw-bold counter-item-text-top"><span class="count" data-count="20000" data-timer="2500">0</span>+</p>
                         <p class="mb-0 counter-item-text-bottom">Warranty Registered Vehicles</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-3 col-6 mb-3 mb-sm-0 counter-item-2">
-                <div class="row">
-                    <div class="col-3 col-sm-2 counter-item-image-area">
+            <div class="col-sm-3 col-12 mb-3 mb-sm-0 counter-item-2">
+                <div class="row justify-content-center">
+                    <div class="col-2 col-sm-2 counter-item-image-area">
                         <img class="img-fluid" src="{{ asset('assets/common/images/icon-2.png') }}" alt="">
                     </div>
-                    <div class="col-9 col-sm-10">
+                    <div class="col-8 col-sm-10">
                         <p class="mb-0 fw-bold counter-item-text-top"><span class="count" data-count="7" data-timer="2000">0</span> LOCATIONS</p>
                         <p class="mb-0 counter-item-text-bottom">Across Sri Lanka</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-3 col-6 mb-3 mb-sm-0 counter-item-3">
-                <div class="row">
-                    <div class="col-3 col-sm-2 counter-item-image-area">
+            <div class="col-sm-3 col-12 mb-3 mb-sm-0 counter-item-3">
+                <div class="row justify-content-center">
+                    <div class="col-2 col-sm-2 counter-item-image-area">
                         <img class="img-fluid" src="{{ asset('assets/common/images/icon-3.png') }}" alt="">
                     </div>
-                    <div class="col-9 col-sm-10">
+                    <div class="col-8 col-sm-10">
                         <p class="mb-0 fw-bold counter-item-text-top"><span class="count" data-count="10000" data-timer="3000">0</span>+</p>
                         <p class="mb-0 counter-item-text-bottom">Vehicles Serviced Monthly</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-3 col-6 mb-3 mb-sm-0 counter-item-4">
+            {{--<div class="col-sm-3 col-6 mb-3 mb-sm-0 counter-item-4">
                 <div class="row">
                     <div class="col-3 col-sm-2 counter-item-image-area">
                         <img class="img-fluid" src="{{ asset('assets/common/images/icon-4.png') }}" alt="">
@@ -44,7 +44,7 @@
                         <p class="mb-0 counter-item-text-bottom">Warranty Programme</p>
                     </div>
                 </div>
-            </div>
+            </div>--}}
         </div>
     </div>
 </div>
